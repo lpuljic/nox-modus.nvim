923e5fbf81ce3638cc4b2dc5b960e15a891f9106
Lano Puljic <lano.puljic@gmail.com>
2023-12-15 11:41:07 +1100

    fix:lsp: symantec highlighting disabled.

--- lua/noxmodus/init.lua
+++ lua/noxmodus/init.lua
@@ -13,6 +13,12 @@ M.setup = function()
   vim.g.colors_name = 'noxmodus'
 
   theme.set_highlights()
+
+  -- Hide all semantic highlights
+  for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+   vim.api.nvim_set_hl(0, group, {})
+  end
+  
 end
 
 return M
