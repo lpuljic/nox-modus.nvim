c6ea605c8833d4af0ce81c65b0927a3034307392
Lano Puljic <lano.puljic@gmail.com>
2024-09-05 15:25:26 +1000

    push

--- lua/nox-modus/init.lua
+++ lua/nox-modus/init.lua
@@ -5,15 +5,22 @@ local theme = require("nox-modus.theme")
 M.setup = function()
 	vim.cmd("hi clear")
 
-	vim.o.background = "dark"
 	if vim.fn.exists("syntax_on") then
 		vim.api.nvim_command("syntax reset")
 	end
 
+	vim.o.background = "dark"
 	vim.o.termguicolors = true
 	vim.g.colors_name = "nox-modus"
 
-	theme.set_highlights()
+	local palette = require("nox-modus.palette")
+	local util = require("nox-modus.util")
+	local groups = require("no-clown-fiesta.groups")
+
+	for _, group in ipairs(groups) do
+		group = group.highlight(palette)
+		util.initialise(group)
+	end
 
 	-- Hide all semantic highlights
 	for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
