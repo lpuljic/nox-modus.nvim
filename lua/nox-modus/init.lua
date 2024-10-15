d968da53bae516ceff4309e603e693dbe4ec77b1
Lano Puljic <lano.puljic@gmail.com>
2024-10-15 20:49:34 +1100

    testing

--- lua/nox-modus/init.lua
+++ lua/nox-modus/init.lua
@@ -1,29 +1,7 @@
-local M = {}
+local config = require("nox-modus.theme")
 
-function M.setup()
-	vim.cmd("hi clear")
-
-	if vim.fn.exists("syntax_on") then
-		vim.api.nvim_command("syntax reset")
-	end
-
-	vim.o.background = "dark"
-	vim.o.termguicolors = true
-	vim.g.colors_name = "nox-modus"
-
-	local palette = require("nox-modus.palette")
-	local util = require("nox-modus.util")
-	local integrations = require("nox-modus.integrations")
-
-	for _, group in ipairs(integrations) do
-		group = group.highlight(palette)
-		util.initialise(group)
-	end
-
-	-- Hide all semantic highlights ( maybe this should be a setting option ????)
-	for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-		vim.api.nvim_set_hl(0, group, {})
-	end
-end
+local M = {
+	setup = config.setup,
+}
 
 return M
